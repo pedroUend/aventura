63dd01a811acccf430368c1d4a182acdfa346d58
Pedro Henrique Trigo gois de Oliveira <00001086897961sp@al.educacao.sp.gov.br>
2024-08-28 13:03:30 -0300

    atualizei o meu index.html

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
-avanca.forEach(button =>)
+avanca.forEach(button =>) 
     button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
         const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
